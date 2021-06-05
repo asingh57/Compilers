d564395392818ec01f8514f7b5b3813056116fdb
Abhi Singh <abhi_johnian@hotmail.com>
2021-06-05 15:48:22 -0700

    main initial commit

--- main.cpp
+++ main.cpp
@@ -10,8 +10,22 @@ using namespace antlr4;
 using namespace std;
 
 
-int main(){
+int main(int argc, char *argv[]){
 
+	std::ifstream stream;
+  	stream.open(argv[1]);
+	ANTLRInputStream input(stream);
+	TigerLexer lexer(&input);
+	CommonTokenStream tokens(&lexer);
 
+	tokens.fill();
+	for (auto token : tokens.getTokens()) {
+	  //std::cout << token->toString() << std::endl;
+	}
+
+  	TigerParser parser(&tokens);
+  	tree::ParseTree* tree = parser.tiger_program();
+
+  	std::cout << tree->toStringTree(&parser) << std::endl << std::endl;
 	return 0;
 }
